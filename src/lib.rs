cba1d90c6c5fbf2770abffa3926cec7be49459b8
Gavin Panella <gavinpanella@gmail.com>
2021-08-23 15:23:32 +0200

    Implement size_hint for iter_non_repeating

--- src/lib.rs
+++ src/lib.rs
@@ -380,6 +380,7 @@ where
     iters: Vec<(ITERATOR, Option<&'a str>)>,
     separator: String,
     capacity: usize,
+    size: Option<usize>,
 }
 
 impl<'a> NamesProduct<'a, core::iter::Cycle<alloc::vec::IntoIter<Option<&'a str>>>> {
@@ -404,6 +405,12 @@ impl<'a> NamesProduct<'a, core::iter::Cycle<alloc::vec::IntoIter<Option<&'a str>
                 .collect(),
             separator: separator.to_string(),
             capacity: Self::capacity(lists, separator),
+            size: match lists {
+                [] => Some(0),
+                ls => ls.iter().fold(Some(1usize), |acc, list| {
+                    acc.and_then(|a| a.checked_mul(list.len()))
+                }),
+            },
         }
     }
 
@@ -432,6 +439,10 @@ where
     ITERATOR: Iterator<Item = Option<&'a str>>,
 {
     type Item = String;
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (self.size.unwrap_or(0), self.size)
+    }
 
     fn next(&mut self) -> Option<Self::Item> {
         let mut bump = true; // Request advance of next iterator.
@@ -467,7 +478,9 @@ where
             // We reached the end of the last iterator, hence we're done.
             None
         } else {
-            // We may be able to construct a word!
+            // Keep track of the number of names remaining.
+            self.size = self.size.map(|s| s.saturating_sub(1));
+            // We may be able to construct a name!
             self.iters.iter().fold(
                 Some(String::with_capacity(self.capacity)),
                 |acc, (_, w)| match (acc, *w) {
